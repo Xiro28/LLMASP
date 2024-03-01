4afc1201bcc63842ec59e54d429b199913d34a98
Lorenzo Grillo <grillo383@gmail.com>
2024-03-01 10:22:04 +0100

    Implemented: ASP atom creation from user request Replaced: OpenAI chatgpt with Gemini (so we can have some google api requests through it) Implemented: Yaml files to config the request

--- src/main.py
+++ src/main.py
@@ -1,48 +1,62 @@
-from openai import OpenAI
-
-
-def loadAPI() -> str:
-    import json
-
-    json_loaded = json.load(open("config/api_key.json"))
-
-    if 'api' not in json_loaded.keys() or json_loaded['api'] is None:
-        raise Exception("API key not valid: config/api_key.json")
-
-    return json_loaded['api']
-
+import valasp
+import g4f
+import yaml
 
 def userInputToGPT(text: str) -> dict:
     return {"role": "user", "content": text}
 
+def loadConfig(path: str) -> dict:
+    return yaml.load(open(path, "r"), Loader=yaml.Loader)
 
-def request_body(user_input: str) -> dict:
-    return userInputToGPT(
-        f"With the following user input [{user_input}] " +
-        "Using your knowlage about ASP, extract the following information:" +
-        "Atoms and function" +
-        "Make sure that all the functions are with the same ariety when called" +
-        "Once you extracted the ASP atoms and functions, give the whole ASP program" +
-        "Without adding further information"
-    )
+def request_body(user_input: str, config: dict) -> dict:
+    if user_input is None:
+        inputs = [x.split('(')[0] for x in config['predicates']['input']]
+        requests = [config['arguments'][x]['request'] for x in inputs]
+        data_types = [config['arguments'][x]['data_type'] for x in inputs]
+
+        return userInputToGPT(
+            f"{requests[0]} and answer with that structure (ASP atoms):"
+            f"{config['predicates']['input'][0]} with the following types {data_types[0]}.\n"
+            f"You MUSTN'T provide the explanation of the output"
+        )
+
+    return userInputToGPT(f"{config['extract_value']} from this '{user_input}' (in ASP atom) with this structure:\n"
+                          f"{config['predicates']['user_request'][0]}.\n"
+                          f"You MUSTN'T provide the explanation of the output")
 
 
-def createGPT() -> "OpenAI.completions":
-    API_KEY = loadAPI()
-    return OpenAI(api_key=API_KEY).chat.completions
-
-
-def makeRequest(client_gpt: "OpenAI.completions", user_input: str) -> str:
-    return client_gpt.create(
+def makeRequest(user_input: str, config: dict) -> tuple:
+    return (g4f.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=request_body(user_input),
-        temperature=1.0  # 0.0 - 2.0
-    ).choices[0].message
+        provider=g4f.Provider.Gemini,
+        messages=[request_body(None, config)],
+        stream=False,
+    ), g4f.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        provider=g4f.Provider.Gemini,
+        messages=[request_body(user_input, config)],
+        stream=False,
+    ))
 
+
+def getCodeFromResponse(response: str) -> str:
+    return response[
+           response.find("```") + 3:
+           response.rfind("```")
+           ]
+
+def cleanOutputRequest(req: str) -> str:
+    if "```" in req :
+        req = getCodeFromResponse(req)
+    if 'asp' in req:
+        req = req.replace('asp', '')
+    return req
 
 def main():
-    client_gpt = createGPT()
-    makeRequest(client_gpt, input("Enter message to be converted to ASP"))
+    config = loadConfig("config.yml")
+    req = makeRequest(input("Enter message to be converted to ASP:\n"), config)
+    code = cleanOutputRequest(req[0]) + cleanOutputRequest(req[1])
+    print(code)
 
 
 if __name__ == "__main__":
